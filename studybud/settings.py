0ffb78077a6ca932c20d8ebb66d8fb3af6798b86
Umst gh <mohmedalhkim@gmail.com>
2022-12-21 10:48:15 +0200

    second commit

--- studybud/settings.py
+++ studybud/settings.py
@@ -126,11 +126,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'images/'
 
-# STATICFILES_DIRS=[
-#     BASE_DIR / 'static'
-# ]
+STATICFILES_DIRS=[
+    BASE_DIR / 'static'
+]
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = BASE_DIR / 'static/images/usersimages'
